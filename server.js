91bfb4375205a78920d8824ae09081bed55fa897
arvas512 <arvas.chr@gmail.com>
2020-03-30 21:09:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ let http = require('http');
 let fs = require('fs');
 
  
-let SF = ( resp, fil) => 
-{ fs.readFile( fil, function (error, data) 
+let SF = ( resp, enc, fil) => 
+{ fs.readFile( fil, enc, function (error, data) 
   { if (error) { resp.writeHead(404); resp.write('Whoops! File not found!'); } 
     else       { resp.write(data); }
     resp.end(); }); }
@@ -12,8 +12,8 @@ let SF = ( resp, fil) =>
 let handleRequest = (req, resp) => { 
 //  console.log('Got a request ' + req.url);
   resp.writeHead(200, { 'Content-Type': 'text/html' });
-       if ( req.url==='/')	    SF( resp, './pages/index.html')
-  else if ( req.url==='/mati.jpg')  SF( resp, './pages/mati.jpg'  )
+       if ( req.url==='/')	    SF( resp, 'utf8', './pages/index.html')
+  else if ( req.url==='/mati.jpg')  SF( resp,     '', './pages/mati.jpg'  )
 };
  
 http.createServer( handleRequest).listen(8080);
